3c3532571d91beda40aa68b07e8e9a2d57fe9b50
eatpopminh <54875208+eatpopminh@users.noreply.github.com>
2020-04-02 01:05:54 -0700

    Got file to transfer over to matrix

---
+++ src/main.java
@@ -0,0 +1,55 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		File f = new File("input.txt");
+		List<String> lines = Files.readAllLines(Paths.get("input.txt"));
+		int num = Integer.parseInt(lines.get(0));
+		System.out.println(lines.size());
+		double [][] myMatrix = new double[num][num];
+		System.out.println(lines.get(1).length()-1);
+
+		int h = 0, k = 0;
+		String temp = "";
+		for(int i = 1;i<num+1;i++)
+		{
+			for(int j = 0; j<lines.get(i).length() ;j++)
+			{
+				if(lines.get(i).toCharArray()[j]==' ')
+				{
+					myMatrix[h][k] = Double.parseDouble(temp.trim());
+					k++;
+					temp = "";
+				}
+				else
+				{
+					temp += lines.get(i).toCharArray()[j];
+				}
+			}
+			k=0;
+			h++;
+		}
+		printMatrix(myMatrix);
+		//System.out.println(Arrays.deepToString(myMatrix).replace("], ", "]\n"));
+	}
+	public static void printMatrix(double matrix[][])
+	{
+		for(int i = 0 ; i< matrix.length ; i++)
+		{
+			for(int j = 0 ; j <matrix[i].length ;j++)
+			{
+				System.out.print(matrix[i][j]);
+				System.out.print(" , ");
+			}
+			System.out.println();
+		}
+	}
+}
